2272c55dfdae679a05e917e6f11effb6bcdec439
tjd6356-afk <tjd6356@gmail.com>
2026-04-30 10:58:26 +0900

    아직 미완

--- plsfood/Assets/script/button.cs
+++ plsfood/Assets/script/button.cs
@@ -1,18 +1,31 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
+using UnityEngine.SceneManagement; // 씬 전환을 위해 필수!
 
 public class button : MonoBehaviour
 {
+    
+    public GameObject helpPanel; 
+
     public void GameStart()
     {
-        SceneManagement.LoadScene("Game_Play")
+        
+        SceneManager.LoadScene("Game_Play");
     }
+
     public void Gametitle()
     {
+        
         SceneManager.LoadScene("frist_page");
     }
 
+    public void OpenHelp()
+    {
+        
+        helpPanel.SetActive(true);
+    }
 
-
+    public void CloseHelp()
+    {
+        helpPanel.SetActive(false);
+    }
 }
